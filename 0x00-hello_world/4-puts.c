2787dac8d66a36eaed638b9f8048f6b6b117c998
TheDhavid <dhavidadeyeye@gmail.com>
2022-10-13 13:07:18 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
 	/* this will print a text using puts() function */
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return 0;
 }
