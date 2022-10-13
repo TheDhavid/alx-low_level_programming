aad5d2a34aa3835ed55a5c7f37c964fe0ab438ec
TheDhavid <dhavidadeyeye@gmail.com>
2022-10-13 14:43:00 -0700

    gad

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-/*
- *main - entry
- *return - 0
+/**
+ *main - prints the phrase with proper grammar,
+ *but the outcome is a piece of art,
+ *Return - 0 if exited properly
  */
 int main(void)
 {
