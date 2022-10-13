78b69fab8ebe11ce584a8ea233ac63bf7dea24f6
TheDhavid <dhavidadeyeye@gmail.com>
2022-10-13 13:49:00 -0700

    test update 11

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*main is entry point for code*/
 
-int main(void);
+int main(void)
 {
 	/* this will print a text using puts() function */
 	puts("\"Programming is like building a multilingual puzzle");
