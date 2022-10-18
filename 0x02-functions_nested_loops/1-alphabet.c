47370132729d3bac7584d4ccd010c7c08d938d02
TheDhavid <dhavidadeyeye@gmail.com>
2022-10-18 16:23:41 -0700

    pool

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#nclude "maun.h"
+#include "maun.h"
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line
@@ -7,7 +7,7 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <+ 'z'; leterr++)
+	for (letter = 'a'; letter <+ 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
