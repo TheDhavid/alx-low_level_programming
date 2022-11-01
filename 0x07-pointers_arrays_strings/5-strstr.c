92ff0903810a15ce92075027a2a79ca7bb10041a
TheDhavid <dhavidadeyeye@gmail.com>
2022-11-01 15:13:53 -0700

    gibberish

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,6 +5,7 @@
 /**
 * _strstr - locates a substring
 *
+*
 * @haystack: the longer string to search
 * @needle: the first occurrence of the substring
 * Return: a pointer beg of substring or @Null if it not foound.
