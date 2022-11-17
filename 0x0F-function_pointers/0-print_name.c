5883d65f137903c28baeff885481624b83aeb241
TheDhavid <dhavidadeyeye@gmail.com>
2022-11-16 16:16:18 -0800

    looptt

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,18 +3,12 @@
 
 
 /**
-
- * print_name - a function that prints a name.
-
- * @name: the name given
-
- * @f: function of name
-
- *
-
- * Rteurn: void
-
- */
+* print_name - a function that prints a name.
+* @name: the name given
+* @f: function of name
+*
+* Rteurn: void
+*/
 
 void print_name(char *name, void (*f)(char *))
 
